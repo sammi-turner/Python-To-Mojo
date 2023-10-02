fb957adefac1ff15081ea09b4448c0a8ec195817
Sammi Turner <ms.sammi.turner@gmail.com>
2023-10-02 02:26:34 +0100

    Create app.py

---
+++ 8kyu_codewars/sum_without_highest_and_lowest/app.py
@@ -0,0 +1,24 @@
+"""
+Sum all the numbers of a given array (list), except the highest and the lowest element (by value, not by index).
+
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+Mind the input validation.
+
+Example :
+
+[6, 2, 1, 8, 10] => 16
+[1, 1, 11, 2, 3] => 6
+
+Input validation :
+
+If an empty value (None) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+"""
+
+def sum_array(arr):
+    if len(arr) > 2:
+        return sum(arr) - max(arr) - min(arr)
+    else:
+        return 0
+
+print(f"{sum_array([6, 2, 1, 8, 10])} {sum_array([1, 1, 11, 2, 3])}")
