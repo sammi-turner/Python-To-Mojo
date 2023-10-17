5c30945f02faf783a5043426aee4cf2097cf658c
Sammi Turner <ms.sammi.turner@gmail.com>
2023-10-17 20:07:53 +0100

    Add files via upload

--- unix_getch_demo/app.mojo
+++ unix_getch_demo/app.mojo
@@ -10,7 +10,12 @@ fn unix_getch() raises -> PythonObject:
     let _v = tty.setcbreak(fd)
     return sys.stdin.read(1)
 
+fn cls() raises:
+    let os = Python.import_module('os')
+    let v = os.system('clear')
+
 fn main() raises:
     print('Press any key...')
     let input_char = unix_getch()
+    cls()
     print('You pressed:', input_char)
--- unix_getch_demo/app.py
+++ unix_getch_demo/app.py
@@ -1,14 +1,16 @@
 # This demo will NOT work natively on Windows!
+import os
 import sys
 import tty
 import termios
 
 def unix_getch() -> str:
     fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
+    v = termios.tcgetattr(fd)
     tty.setcbreak(fd)
     return sys.stdin.read(1)
 
 print('Press any key...')
 input_char = unix_getch()
+os.system('clear')
 print('You pressed:', input_char)
