bc0ba17f998d16cefd7a8ffc89570bd734800a98
Sammi Turner <ms.sammi.turner@gmail.com>
2023-10-27 21:22:28 +0100

    Update app.py

--- 6kyu_codewars/almost_even/app.py
+++ 6kyu_codewars/almost_even/app.py
@@ -5,7 +5,9 @@ The sum of the parts should be the original value, but each part should be an in
 
 Complete the function so that it returns an array of integers representing the parts. 
 
-If the input number is too small to split it into requested amount of parts, the additional parts should have value 0. Ignoring the order of the parts, there is only one valid solution for each input to your function!
+If the input number is too small to split it into requested amount of parts, the additional parts should have value 0. 
+
+Ignoring the order of the parts, there is only one valid solution for each input to your function!
 
 Example:
 
